574afd216bf71952daa0376ea4c0c3e54c60c75c
Hoshin <martin.bahier@gmail.com>
2015-04-06 19:34:21 +0200

    deleted unneccessary wkhtmltopdf call

--- util/generatePdfs.sh
+++ util/generatePdfs.sh
@@ -2,8 +2,6 @@
 
 #deployment + pdf-ization of resumes
 
-wkhtmltopdf ./cv_fr.html cv_mbahier_fr.pdf
-
 for localization in fr en; do
     wkhtmltopdf ./cv_${localization}.html ./cv_mbahier_${localization}.pdf
 done
