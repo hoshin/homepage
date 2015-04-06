443d4bb690520e68e3a415dff38fb5399e5c2c0f
Hoshin <martin.bahier@gmail.com>
2015-04-06 19:39:47 +0200

    Added some help + the abolity to select a specific directory where the files to be processed will be

--- util/generatePdfs.sh
+++ util/generatePdfs.sh
@@ -2,6 +2,14 @@
 
 #deployment + pdf-ization of resumes
 
-for localization in fr en; do
-    wkhtmltopdf ./cv_${localization}.html ./cv_mbahier_${localization}.pdf
-done
+if [ -d "$1" ]; then
+    cd $1
+    for localization in fr en; do
+        echo "Generating pdf for lang ${localization}"
+        wkhtmltopdf ./cv_${localization}.html ./cv_mbahier_${localization}.pdf
+    done
+    cd -
+else 
+    echo "You need to specify a valid directory to switch to"
+    echo "Usage : ./generatePdfs.sh ./someDirectory"
+fi
